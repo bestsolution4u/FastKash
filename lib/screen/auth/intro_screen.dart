8a38fb00d5ffc088a544c9f537863b2ab7587264
MobileHorse <58142664+MobileHorse@users.noreply.github.com>
2021-05-03 12:54:49 +0300

    Initial Version

--- lib/screen/auth/intro_screen.dart
+++ lib/screen/auth/intro_screen.dart
@@ -8,21 +8,34 @@ class IntroScreen extends StatefulWidget {
 }
 
 class _IntroScreenState extends State<IntroScreen> {
-
   @override
   void initState() {
     super.initState();
-
   }
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-        statusBarColor: AppTheme.purple,
-      statusBarIconBrightness: Brightness.light
-    ));
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: AppTheme.purple, statusBarIconBrightness: Brightness.light));
     return Scaffold(
       backgroundColor: AppTheme.purple,
+      body: Stack(
+        children: [
+          Positioned(
+            bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                height: MediaQuery.of(context).size.height / 2,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(50),
+                      topLeft: Radius.circular(50)),
+                ),
+              ))
+        ],
+      ),
     );
   }
 }
